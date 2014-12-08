3040422d07c2b4ab27ebdcbf07d58098ecc91fd2
Chris Hou <yutao@ChrisHou.local>
2014-12-08 10:47:35 +0800

    add blur.c source code

---
+++ StackBlur/src/main/blurc/obj/local/armeabi-v7a/objs/blur/blur.o.d
@@ -0,0 +1,65 @@
+/Users/yutao/development/yutao-git/github/android-stackblur/StackBlur/src/main/blurc/obj/local/armeabi-v7a/objs/blur/blur.o: \
+ /Users/yutao/development/yutao-git/github/android-stackblur/StackBlur/src/main/blurc/jni/blur.c \
+ /Users/yutao/package/android-ndk-r9/platforms/android-8/arch-arm/usr/include/jni.h \
+ /Users/yutao/package/android-ndk-r9/platforms/android-8/arch-arm/usr/include/string.h \
+ /Users/yutao/package/android-ndk-r9/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ /Users/yutao/package/android-ndk-r9/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/yutao/package/android-ndk-r9/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ /Users/yutao/package/android-ndk-r9/platforms/android-8/arch-arm/usr/include/malloc.h \
+ /Users/yutao/package/android-ndk-r9/platforms/android-8/arch-arm/usr/include/stdio.h \
+ /Users/yutao/package/android-ndk-r9/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ /Users/yutao/package/android-ndk-r9/platforms/android-8/arch-arm/usr/include/stdint.h \
+ /Users/yutao/package/android-ndk-r9/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ /Users/yutao/package/android-ndk-r9/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ /Users/yutao/package/android-ndk-r9/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ /Users/yutao/package/android-ndk-r9/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ /Users/yutao/package/android-ndk-r9/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ /Users/yutao/package/android-ndk-r9/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ /Users/yutao/package/android-ndk-r9/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ /Users/yutao/package/android-ndk-r9/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ /Users/yutao/package/android-ndk-r9/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ /Users/yutao/package/android-ndk-r9/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/yutao/package/android-ndk-r9/platforms/android-8/arch-arm/usr/include/android/log.h \
+ /Users/yutao/package/android-ndk-r9/platforms/android-8/arch-arm/usr/include/android/bitmap.h
+
+/Users/yutao/package/android-ndk-r9/platforms/android-8/arch-arm/usr/include/jni.h:
+
+/Users/yutao/package/android-ndk-r9/platforms/android-8/arch-arm/usr/include/string.h:
+
+/Users/yutao/package/android-ndk-r9/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/yutao/package/android-ndk-r9/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/yutao/package/android-ndk-r9/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+/Users/yutao/package/android-ndk-r9/platforms/android-8/arch-arm/usr/include/malloc.h:
+
+/Users/yutao/package/android-ndk-r9/platforms/android-8/arch-arm/usr/include/stdio.h:
+
+/Users/yutao/package/android-ndk-r9/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+/Users/yutao/package/android-ndk-r9/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+/Users/yutao/package/android-ndk-r9/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+/Users/yutao/package/android-ndk-r9/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+/Users/yutao/package/android-ndk-r9/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/yutao/package/android-ndk-r9/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+/Users/yutao/package/android-ndk-r9/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+/Users/yutao/package/android-ndk-r9/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/yutao/package/android-ndk-r9/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+/Users/yutao/package/android-ndk-r9/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+/Users/yutao/package/android-ndk-r9/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+/Users/yutao/package/android-ndk-r9/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/yutao/package/android-ndk-r9/platforms/android-8/arch-arm/usr/include/android/log.h:
+
+/Users/yutao/package/android-ndk-r9/platforms/android-8/arch-arm/usr/include/android/bitmap.h:
---
+++ StackBlur/src/main/blurc/obj/local/x86/objs/blur/blur.o.d
@@ -0,0 +1,68 @@
+/Users/yutao/development/yutao-git/github/android-stackblur/StackBlur/src/main/blurc/obj/local/x86/objs/blur/blur.o: \
+ /Users/yutao/development/yutao-git/github/android-stackblur/StackBlur/src/main/blurc/jni/blur.c \
+ /Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/jni.h \
+ /Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/string.h \
+ /Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/sys/cdefs.h \
+ /Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/sys/cdefs_elf.h \
+ /Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/android/api-level.h \
+ /Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/malloc.h \
+ /Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/stdio.h \
+ /Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/sys/types.h \
+ /Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/stdint.h \
+ /Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/sys/_types.h \
+ /Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/machine/_types.h \
+ /Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/linux/posix_types.h \
+ /Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/linux/stddef.h \
+ /Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/linux/compiler.h \
+ /Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/asm/posix_types.h \
+ /Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/asm/posix_types_32.h \
+ /Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/asm/types.h \
+ /Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/linux/types.h \
+ /Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/machine/kernel.h \
+ /Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/sys/sysmacros.h \
+ /Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/android/log.h \
+ /Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/android/bitmap.h
+
+/Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/jni.h:
+
+/Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/string.h:
+
+/Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/sys/cdefs.h:
+
+/Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/sys/cdefs_elf.h:
+
+/Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/android/api-level.h:
+
+/Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/malloc.h:
+
+/Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/stdio.h:
+
+/Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/sys/types.h:
+
+/Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/stdint.h:
+
+/Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/sys/_types.h:
+
+/Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/machine/_types.h:
+
+/Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/linux/posix_types.h:
+
+/Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/linux/stddef.h:
+
+/Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/linux/compiler.h:
+
+/Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/asm/posix_types.h:
+
+/Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/asm/posix_types_32.h:
+
+/Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/asm/types.h:
+
+/Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/linux/types.h:
+
+/Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/machine/kernel.h:
+
+/Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/sys/sysmacros.h:
+
+/Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/android/log.h:
+
+/Users/yutao/package/android-ndk-r9/platforms/android-9/arch-x86/usr/include/android/bitmap.h:
